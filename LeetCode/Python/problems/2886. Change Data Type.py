163676ff073b723238ccf4e5cdbde542dda32f9c
Ernest-Barr <embarrington3@gmail.com>
2026-04-10 11:00:10 -0400

    4/10/26: Problems 2878-2891 Intro to Pandas

---
+++ LeetCode/Python/problems/2886. Change Data Type.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def changeDatatype(students: pd.DataFrame) -> pd.DataFrame:
+    students["grade"] = pd.to_numeric(students["grade"], downcast="integer")
+
+    return students
