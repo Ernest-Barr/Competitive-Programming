2f07fe4b7751c8dbb1be5b0427d51eb6c9c40cd0
Ernest-Barr <embarrington3@gmail.com>
2026-04-21 19:03:31 -0400

    4/21/26: 163. Missing Ranges

---
+++ LeetCode/Kotlin/src/163. Missing Ranges.kt
@@ -0,0 +1,17 @@
+private fun findMissingRanges(
+    nums: IntArray,
+    lower: Int,
+    upper: Int,
+): List<List<Int>> {
+    val n = nums.size
+    val res: MutableList<List<Int>> = mutableListOf()
+    if (n == 0) return listOf(listOf(lower, upper))
+
+    if (nums[0] != lower) res.add(listOf(lower, nums[0] - 1))
+
+    for (i in 1..<n) if (nums[i] != nums[i - 1] + 1) res.add(listOf(nums[i - 1] + 1, nums[i] - 1))
+
+    if (nums[n - 1] != upper) res.add(listOf(nums[n - 1] + 1, upper))
+
+    return res
+}
